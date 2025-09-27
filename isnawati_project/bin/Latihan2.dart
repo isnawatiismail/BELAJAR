9b633c47bc1547704643c6df3cd27c3e9c04b3ca
isnawatiismail <isnawati042023@gmail.com>
2025-09-27 20:50:11 +0700

    Tugas 2 Latihan 2

---
+++ isnawati_project/bin/Latihan2.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Simpan panjang dan lebar pada variabel bertipe double
+  double panjang = 10;
+  double lebar = 5;
+
+  // Hitung luas dan keliling
+  double luas = panjang * lebar;
+  double keliling = 2 * (panjang + lebar);
+
+  // Tampilkan hasil
+  print("=== Perhitungan Persegi Panjang ===");
+  print("Panjang  = $panjang");
+  print("Lebar    = $lebar");
+  print("Luas     = $luas");
+  print("Keliling = $keliling");
+}
